d6ca0e1c276a056aa33160ab9e6ca82b52c3c834
Aleksandra Druciak <40007575+Drucia@users.noreply.github.com>
2020-10-10 23:35:52 +0200

    Update agent.js
    .

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'http://10.0.0.21:5000/api';
+const API_ROOT = 'http://10.0.0.21:5000';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
